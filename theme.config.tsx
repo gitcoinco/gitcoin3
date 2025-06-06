1e332f8f44d728692c3238f944aa4c1e4e910f40
Kevin Owocki <kevin@supermodular.xyz>
2025-06-06 12:51:56 -0600

    domains

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,9 +4,9 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Gitcoin 3.0</span>,
   project: {
-    link: 'https://github.com/gitcoinco/gitcoin3/tree/main',
+    link: 'https://github.com/gitcoinco/gitcoin3/',
   },
-  docsRepositoryBase: 'https://github.com/gitcoinco/gitcoin3/tree/main',
+  docsRepositoryBase: 'https://github.com/gitcoinco/gitcoin3/tree/master',
   footer: {
     text: 'Gitcoin 3.0',
   },
